65d77828ec13a611f95acd257a836825f507ef32
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-07-05 19:16:49 +0900

    :sparkles: user-email 추가

--- src/main/kotlin/com/spring/localparking/user/domain/User.kt
+++ src/main/kotlin/com/spring/localparking/user/domain/User.kt
@@ -14,6 +14,8 @@ class User (
     var provider: Provider,
     var providerId : Long,
     var nickname: String,
+    @Column(nullable = false, unique = true)
+    var email: String,
     @Enumerated(EnumType.STRING)
     var role : Role
 ){
@@ -21,13 +23,14 @@ class User (
     var id: Long?= null
 
     companion object {
-        fun ofProvider(provider: Provider, providerId: Long, nickname: String): User =
+        fun ofProvider(provider: Provider, providerId: Long, nickname: String, email: String): User =
             User(
                 provider = provider,
                 providerId = providerId,
                 nickname = nickname,
+                email = email,
                 role = Role.USER
             )
     }
-    constructor() : this(null, null, Provider.KAKAO, 0L, "", Role.USER)
+    constructor() : this(null, null, Provider.KAKAO, 0L, "", "", Role.USER)
 }
