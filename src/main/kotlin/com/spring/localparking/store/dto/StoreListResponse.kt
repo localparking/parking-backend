b316f8d388ffb2fc8054e1587d45889977de6b4f
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-07-26 01:58:21 +0900

    :bug: nullable 버그 수정

--- src/main/kotlin/com/spring/localparking/store/dto/StoreListResponse.kt
+++ src/main/kotlin/com/spring/localparking/store/dto/StoreListResponse.kt
@@ -1,11 +1,12 @@
 package com.spring.localparking.store.dto
 
+import com.spring.localparking.global.dto.StoreType
 import com.spring.localparking.store.domain.StoreDocument
 
 data class StoreListResponse(
     val storeId: Long,
     val name: String,
-    val storeType: String,
+    val storeType: String? = StoreType.GENERAL.name,
     val categoryNames: List<String>,
     val lat: Double,
     val lon: Double,
@@ -16,7 +17,7 @@ data class StoreListResponse(
             return StoreListResponse(
                 storeId = doc.id,
                 name = doc.name,
-                storeType = doc.storeType.name,
+                storeType = doc.storeType?.name,
                 categoryNames = doc.categoryNames,
                 lat = doc.location.lat,
                 lon = doc.location.lon,
