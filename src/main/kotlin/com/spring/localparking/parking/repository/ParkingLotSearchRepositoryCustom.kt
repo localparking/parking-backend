2ad8099e108757c225d98f143f8d1a44d3566df7
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-07-29 22:16:09 +0900

    :sparkles: 지도기반 줌 레벨 -> 검색 조회 텍스트 검색 -> 2에서 10까지

--- src/main/kotlin/com/spring/localparking/parking/repository/ParkingLotSearchRepositoryCustom.kt
+++ src/main/kotlin/com/spring/localparking/parking/repository/ParkingLotSearchRepositoryCustom.kt
@@ -2,18 +2,19 @@ package com.spring.localparking.parking.repository
 
 import com.spring.localparking.parking.domain.ParkingLotDocument
 import com.spring.localparking.parking.dto.ParkingLotSearchRequest
-import com.spring.localparking.store.domain.StoreDocument
-import com.spring.localparking.store.dto.StoreSearchRequest
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ParkingLotSearchRepositoryCustom {
-    fun searchByFilters(request: ParkingLotSearchRequest, pageable: Pageable): Page<ParkingLotDocument>
+    fun searchByFilters(request: ParkingLotSearchRequest,
+                        pageable: Pageable,
+                        searchRadiusKm: Int): Page<ParkingLotDocument>
 
     fun searchByText(
         request: ParkingLotSearchRequest,
-        pageable: Pageable
+        pageable: Pageable,
+        searchRadiusKm: Int
     ): Page<ParkingLotDocument>
 }
