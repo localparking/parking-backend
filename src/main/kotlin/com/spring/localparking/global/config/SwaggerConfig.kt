3fcf8f7d9ced26ccbed7fa086690c38daedf6641
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-08-18 19:21:07 +0900

    :zap: 스웨거 그룹 분리

--- src/main/kotlin/com/spring/localparking/global/config/SwaggerConfig.kt
+++ src/main/kotlin/com/spring/localparking/global/config/SwaggerConfig.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -34,5 +35,53 @@ class SwaggerConfig {
                 )
             )
             .security(listOf(securityRequirement))
+    }
+    @Bean
+    fun commonApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("common")
+            .displayName("일반")
+            .pathsToMatch(
+                "/store/**",
+                "/parking/**",
+                "/category/**",
+                "/text-search/**"
+            )
+            .build()
+    }
+    @Bean
+    fun userApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("user")
+            .displayName("사용자")
+            .pathsToMatch(
+                "/user/**",
+                "/auth/**",
+                "/register/**",
+                "/onboarding/**",
+                "/order/**"
+            )
+            .build()
+    }
+    @Bean
+    fun storekeeperApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("storekeeper")
+            .displayName("점주")
+            .pathsToMatch(
+                "/storekeeper/**"
+            )
+            .build()
+    }
+
+    @Bean
+    fun adminApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("admin")
+            .displayName("관리자")
+            .pathsToMatch(
+                "/admin/**"
+            )
+            .build()
     }
 }
